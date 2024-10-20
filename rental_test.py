a335c67331c04ec55009a5fedbce84babd7e716b
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-10-20 20:07:20 +0700

    Modify rental test to get movie from MovieCatalog and write docstrings.

--- rental_test.py
+++ rental_test.py
@@ -1,23 +1,18 @@
 import unittest
-from customer import Customer
+from movie_catalog import MovieCatalog
 from rental import Rental
-from movie import Movie
 
 
 class RentalTest(unittest.TestCase):
-
+    """Test cases for the Rental class."""
     def setUp(self):
-        self.new_movie = Movie("Dune: Part Two", Movie.NEW_RELEASE)
-        self.regular_movie = Movie("Air", Movie.REGULAR)
-        self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
-
-    def test_movie_attributes(self):
-        """trivial test to catch refactoring errors or change in API of Movie"""
-        m = Movie("Air", Movie.REGULAR)
-        self.assertEqual("Air", m.get_title())
-        self.assertEqual(Movie.REGULAR, m.get_price_code())
+        """Set up a MovieCatalog instance and sample movies for testing."""
+        self.new_movie = MovieCatalog().get_movie("Dune: Part Two")
+        self.regular_movie = MovieCatalog().get_movie("The Batman")
+        self.childrens_movie = MovieCatalog().get_movie("Turning Red")
 
     def test_rental_price(self):
+        """Test that the rental price is calculated correctly."""
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.get_price(), 3.0)
         rental = Rental(self.new_movie, 5)
